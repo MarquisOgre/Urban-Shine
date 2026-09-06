79511f06a34c863c6a0b9ee5bb502bb0df8528bc
Marquis Ogre <marquisogre@gmail.com>
2026-09-06 20:10:32 +0530

    Product Images updated

--- src/data/productImages.ts
+++ src/data/productImages.ts
@@ -1,16 +1,16 @@
-import acid from "@/assets/products/acid.jpg";
-import balm from "@/assets/products/balm.jpg";
-import copper from "@/assets/products/copper-cleaning-liquid.jpg";
-import detergentPowder from "@/assets/products/detergent-powder.jpg";
-import dishWash from "@/assets/products/dish-wash.jpg";
-import floorCleaner from "@/assets/products/floor-cleaner.jpg";
-import handWash from "@/assets/products/hand-wash.jpg";
-import liquidDetergent from "@/assets/products/liquid-detergent.jpg";
-import phenyl from "@/assets/products/phenyl.jpg";
-import roseWater from "@/assets/products/rose-water.jpg";
-import soapOil from "@/assets/products/soap-oil.jpg";
-import toiletCleaner from "@/assets/products/toilet-cleaner.jpg";
-import vaseline from "@/assets/products/vaseline.jpg";
+import acid from "@/assets/products/Acid-Cleaner.png";
+import balm from "@/assets/products/Balm.png";
+import copper from "@/assets/products/Copper-Cleaner.png";
+import detergentPowder from "@/assets/products/Detergent-Powder.png";
+import dishWash from "@/assets/products/Dish-Wash.png";
+import floorCleaner from "@/assets/products/Floor-Cleaner.png";
+import handWash from "@/assets/products/Hand-Wash.png";
+import liquidDetergent from "@/assets/products/Liquid-Detergent.png";
+import phenyl from "@/assets/products/Phenyl.png";
+import roseWater from "@/assets/products/Rose-Water.png";
+import soapOil from "@/assets/products/Soap-Oil.png";
+import toiletCleaner from "@/assets/products/Toilet-Cleaner.png";
+import vaseline from "@/assets/products/Vaseline.png";
 
 export const productImages: Record<string, string> = {
   acid,
